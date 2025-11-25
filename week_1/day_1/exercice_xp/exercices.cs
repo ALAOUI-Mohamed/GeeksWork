45c1e51938a2d684f2716dea16b033868f8ac416
ALAOUI Mohamed <mohamed.al@mobiarchitects.com>
2025-11-25 09:59:46 +0100

    first commit

---
+++ week_1/day_1/exercice_xp/exercices.cs
@@ -0,0 +1,108 @@
+using System;
+
+//Exercice 1: Welcome to C# Programming!
+/*class Exercise1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to C# Programming!");
+    }
+}*/
+
+//Exercice 2: Name showing
+/*class Exercise2
+{
+
+    static void Main(string[] args)
+    {
+        string name = "Alaoui Mohamed";
+        int age = 23;
+        Console.WriteLine($"My name is {name} I am {age} years old.");
+    }
+}*/
+
+//Exercice 3: Simple Calculator
+/*class Exercise3
+{
+
+    static void Main(string[] args)
+    {
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1 + num2;
+        Console.WriteLine($"The sum of {num1} and {num2} is {sum}.");
+    }
+}*/
+
+//Exercice 4: Age Checker
+/*class Exercise4
+{
+
+    static void Main(string[] args)
+    {
+        int age = 20;
+        if (age >= 18)
+        {
+            Console.WriteLine("Access Granted.");
+        }
+        else
+        {
+            Console.WriteLine("Access Denied.");
+        }
+    }
+}*/
+
+//Exercice 5: The countdown while loop
+/*class Exercise5
+{
+
+    static void Main(string[] args)
+    {
+        int count = 10;
+        while (count > 0)
+        {
+            Console.WriteLine(count);
+            count--;
+        }
+        Console.WriteLine("Liftoff!");
+    }
+}*/
+
+//Exercice 6: The Repeater
+/*class Exercise6
+{
+    static void SayHello(string name)
+    {
+        Console.WriteLine("Hello, " + name + "!");
+    }
+
+    static void Main(string[] args)
+    {
+        SayHello("Meliodas");
+        SayHello("Elizabeth");
+        SayHello("Ban");
+    }
+}*/
+
+//Exercice 7: Even or Odd?
+/*class Exercise7
+{
+    static void EvenOrOdd(int number)
+    {
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"{number} is even.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is odd.");
+        }
+    }
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            EvenOrOdd(i);
+        }
+    }
+}*/
