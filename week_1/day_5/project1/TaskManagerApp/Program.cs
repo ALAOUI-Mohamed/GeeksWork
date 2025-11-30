28a250e35d704611cfa24c58791f76e2d43cf511
ALAOUI Mohamed <mohamed.al@mobiarchitects.com>
2025-11-30 19:23:03 +0100

    Added main menu structure in Program.cs

--- week_1/day_5/project1/TaskManagerApp/Program.cs
+++ week_1/day_5/project1/TaskManagerApp/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+
+namespace TaskManagerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+
+            while (running)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to Your Task Manager!");
+                Console.WriteLine("1. Add a Task");
+                Console.WriteLine("2. View Tasks");
+                Console.WriteLine("3. Update Task Status");
+                Console.WriteLine("4. Delete a Task");
+                Console.WriteLine("5. Exit");
+                Console.Write("\nChoose an option: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        running = false;
+                        break;
+                }
+            }
+        }
+    }
+}
