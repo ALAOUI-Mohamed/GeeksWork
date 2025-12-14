14cebafa76578f24fff2efcd4216fdbad3da6fe5
ALAOUI Mohamed <mohamed.al@mobiarchitects.com>
2025-12-14 14:18:29 +0100

    torii gate 2.0

--- week_1/day_4/ATM/machine/obj/Debug/net10.0/ATM.AssemblyInfo.cs
+++ week_1/day_4/ATM/machine/obj/Debug/net10.0/ATM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc3bbb59d15b4e96345876f794566f339c1f8d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9322f9f12a2c5f2cb6aa90e6afab095db0634f25")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
