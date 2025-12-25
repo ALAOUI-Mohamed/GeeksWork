72f9821ed820050350086bb84f38fb1be1c77947
ALAOUI Mohamed <mohamed.al@mobiarchitects.com>
2025-12-25 10:55:41 +0100

    bellcrank

--- week_1/day_4/ATM/machine/obj/Debug/net10.0/ATM.AssemblyInfo.cs
+++ week_1/day_4/ATM/machine/obj/Debug/net10.0/ATM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df7eda177903969aad4c8b92272b1bad414afc71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec8fc01900bbe9cb5c4404eed209b52f7d40226")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
