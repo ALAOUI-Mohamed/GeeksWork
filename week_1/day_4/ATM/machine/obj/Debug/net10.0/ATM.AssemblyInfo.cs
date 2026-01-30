ef3fa08ebc931b5ec84052f07a1b566bb55aea39
ALAOUI Mohamed <mohamed.al@mobiarchitects.com>
2026-01-30 19:44:12 +0100

    lastest updates commit

--- week_1/day_4/ATM/machine/obj/Debug/net10.0/ATM.AssemblyInfo.cs
+++ week_1/day_4/ATM/machine/obj/Debug/net10.0/ATM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec8fc01900bbe9cb5c4404eed209b52f7d40226")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8749d5caf808c8d1c53f5b0eea1383e65ddd9187")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
